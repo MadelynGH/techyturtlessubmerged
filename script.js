2798c8814e94b4ac15f00dbfbbcb2613cef6181c
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2024-10-18 18:36:37 -0400

    Add About page, customize selection style

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.getElementById("header").innerHTML = `
 <a href="index.html"><img src="Techy Turtles Front Logo (Final Version).png" height="80px" id="logo"></a>
-<a href="dead-zones.html" class="header-link">Dead Zones</a> <a href="game.html" class="header-link">Game</a>`;
+<a href="about.html" class="header-link">About</a>
+<a href="dead-zones.html" class="header-link">Dead Zones</a>
+<a href="game.html" class="header-link">Game</a>`;
