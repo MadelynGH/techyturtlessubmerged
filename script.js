838f10b54547b4302f76ae2561b6fea14afec196
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2024-10-18 18:09:58 -0400

    Fix link issues

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("header").innerHTML = `
+<a href="index.html"><img src="Techy Turtles Front Logo (Final Version).png" height="80px" id="logo"></a>
+<a href="dead-zones.html" class="header-link">Dead Zones</a> <a href="game.html" class="header-link">Game</a>`;
