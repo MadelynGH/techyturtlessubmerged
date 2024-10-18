a1d3c048f38147a2d1e0346b28505b5518f37f15
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2024-10-18 15:59:53 -0400

    Create Dead Zones and Game pages (actual game not there yet)

---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+document.getElementById("header").innerHTML = `
+<a href="/"><img src="/images/Techy Turtles Front Logo (Final Version).png" height="80px" id="logo"></a>
+<a href="/dead-zones/" class="header-link">Dead Zones</a> <a href="/game/" class="header-link">Game</a>`;
