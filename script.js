0a4fa5e6db1df9163c88023a98c8e964a1bd8e42
won-eye <aarnashah.hde@gmail.com>
2024-10-20 19:49:54 -0400

    added tips and tricks to help the cause

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ document.getElementById("header").innerHTML = `
 <a href="index.html"><img src="Techy Turtles Front Logo (Final Version).png" height="80px" id="logo"></a>
 <a href="about.html" class="header-link">About</a>
 <a href="dead-zones.html" class="header-link">Dead Zones</a>
-<a href="game.html" class="header-link">Game</a>`;
+<a href="game.html" class="header-link">Game</a>
+<a href="Tips and tricks to help the cause.html" class="header-link">Tips and tricks to help the cause</a>`;
+
