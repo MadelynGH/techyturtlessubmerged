8c954f2a5ac0ff8e82d5deafffff8818507a3dd3
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2024-10-20 13:57:27 -0400

    Add favicon, make game iframe bigger, grow font size for header links

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+document.head.innerHTML = document.head.innerHTML + `
+<link rel="icon" type="image/x-icon" href="favicon.svg">`
 document.getElementById("header").innerHTML = `
 <a href="index.html"><img src="Techy Turtles Front Logo (Final Version).png" height="80px" id="logo"></a>
 <a href="about.html" class="header-link">About</a>
