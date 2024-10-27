70dba727844f1b6c8552c47879a19505ceae3bd0
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2024-10-27 15:50:23 -0400

    Add meta tags

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.head.innerHTML = document.head.innerHTML + `
+<meta charset="UTF-8">
+<meta name="description" content="Help protect our oceans and stop hypoxia.">
+<meta name="keywords" content="dead zones, ocean, conservation, game, enviroment, techy turtles, 66569">
+<meta name="author" content="Techy Turtles">
 <link rel="icon" type="image/x-icon" href="favicon.svg">`
 document.getElementById("header").innerHTML = `
 <a href="index.html"><img src="Techy Turtles Front Logo (Final Version).png" height="80px" id="logo"></a>
