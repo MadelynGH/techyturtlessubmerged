07dcd7d3e42fe5326e7a40f29689b4f2e6fb6b01
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2024-10-27 16:18:50 -0400

    Add new favicon

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ document.head.innerHTML = document.head.innerHTML + `
 <meta name="description" content="Help protect our oceans and stop hypoxia.">
 <meta name="keywords" content="dead zone, hypoxia, nutrient pollution, education ocean, conservation, game, enviroment, techy turtles, 66569">
 <meta name="author" content="Techy Turtles">
-<link rel="icon" type="image/x-icon" href="favicon.svg">`
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">`
 document.getElementById("header").innerHTML = `
 <a href="index.html"><img src="Techy Turtles Front Logo (Final Version).png" height="80px" id="logo"></a>
 <a href="about.html" class="header-link">About</a>
