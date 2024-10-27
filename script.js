7a761e2785d2e428e390d0f411da5155cba4ed63
MadelynGH <91385745+MadelynGH@users.noreply.github.com>
2024-10-27 15:55:01 -0400

    Update meta keyword tag

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.head.innerHTML = document.head.innerHTML + `
 <meta charset="UTF-8">
 <meta name="description" content="Help protect our oceans and stop hypoxia.">
-<meta name="keywords" content="dead zones, ocean, conservation, game, enviroment, techy turtles, 66569">
+<meta name="keywords" content="dead zone, hypoxia, nutrient pollution, education ocean, conservation, game, enviroment, techy turtles, 66569">
 <meta name="author" content="Techy Turtles">
 <link rel="icon" type="image/x-icon" href="favicon.svg">`
 document.getElementById("header").innerHTML = `
